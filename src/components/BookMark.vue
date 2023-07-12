b5362f677f52ca00b703250fc18094b7fda4d984
LeHuyHung_20194579 <hung.lh194579@sis.hust.edu.vn>
2023-07-12 12:56:04 +0700

    fix bookmark

--- src/components/BookMark.vue
+++ src/components/BookMark.vue
@@ -143,7 +143,7 @@ export default {
                       <div><font-awesome-icon style="color: #805D49;" icon="clock" /> {{ todo.shop.time_open }}-{{
                         todo.shop.time_close }} -
                         <span style="font-weight: 700;">
-                          オープン中 - 座席数: {{ !todo.curr_seat ? 0 : todo.curr_seat }}/{{ !todo.max_seat ? 0 : todo.max_seat }}
+                          オープン中 - 座席数: {{ !todo.shop.curr_seat ? 0 : todo.shop.curr_seat }}/{{ !todo.shop.max_seat ? 0 : todo.shop.max_seat }}
                         </span>
                       </div>
                     </div>
