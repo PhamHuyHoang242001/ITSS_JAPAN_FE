ef477c5891721e315f47dd8ea0fc34cf0ef9eb7b
trinhbang <huybang.trinh@secomus.com>
2023-07-12 18:13:38 +0700

    new-fix

--- src/components/AdminNav.vue
+++ src/components/AdminNav.vue
@@ -50,42 +50,41 @@ export default {
 </script>
 <template>
 
-  <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600" style="background-color:#805d49">
+  <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600" style="background-color:#805d49;height:92px">
     <div class="flex flex-wrap items-center justify-between mx-auto p-4">
-     
-        <img src="/chillcafe.png" class="h-8" style="height: 60px;width: 60px" alt="Chill Cafe" />
-    
-      <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
+      <router-link :to="{ name: 'home' }" class="flex items-center mr-0">
+        <img src="/chillcafe.png" class="h-8"  alt="Chill Cafe" style="height: 60px;width: 60px"/>
+      </router-link>
+      <span class="self-center text-3xl font-semibold whitespace-nowrap dark:text-white"
         style="color: #ffffff;">ChillCafe</span>
-
       <img style="height: 60px;width: 60px" id="avatarButton" type="button" data-dropdown-toggle="userDropdown"
         data-dropdown-placement="bottom-start" class="w-10 h-10 rounded-full cursor-pointer"
         src="https://cdn-icons-png.flaticon.com/512/6596/6596121.png" alt="User dropdown" />
 
       <!-- Dropdown menu -->
       <div id="userDropdown"
-        class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-        <div class="px-4 py-3 text-sm text-gray-900 dark:text-white">
+        class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-60  dark:bg-gray-700 dark:divide-gray-600" style="border: 1px solid #000; padding:10px; ">
+        <div class="px-4 py-3 text-sm text-gray-900 dark:text-white" style="background-color: #36ABFF; border-radius:5px;text-align:center;">
           <div>{{ user.name }}</div>
         </div>
         <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="avatarButton">
-          <router-link :to="{ name: 'approve' }">
-            <div class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">ダッシュボード </div>
+          <router-link :to="{ name: 'adminhome' }">
+            <div class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">承認されたリスト</div>
           </router-link>
         </ul>
         <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="avatarButton">
-          <router-link :to="{ name: 'adminhome' }">
-            <div class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">リクエスト一覧 </div>
+          <router-link :to="{ name: 'approve' }">
+            <div class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">リクエスト一覧</div>
           </router-link>
         </ul>
         <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="avatarButton">
           <router-link :to="{ name: 'addsubadmin' }">
-            <div class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">ユーザーサインアップ </div>
+            <div class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">ユーザーサインアップ</div>
           </router-link>
         </ul>
         <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="avatarButton">
           <router-link :to="{ name: 'usermanagement' }">
-            <div class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">ユーザーリスト </div>
+            <div class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">ユーザーリスト</div>
           </router-link>
         </ul>
         <div class="py-1">
